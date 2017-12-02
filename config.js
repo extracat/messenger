3c1cc7260d7bb3727e535b9e59ad9853a77ea421
Extracat <snezhi@MBP-Sabina.Dlink>
2017-12-02 14:32:21 +0300

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secret': '12345',
+    'secret': 'supertonyhasasecret',
     'database': 'mongodb://heroku_hcg44bn0:8pj4rnnf5efr4l381oivom969d@ds127506.mlab.com:27506/heroku_hcg44bn0'
 };
