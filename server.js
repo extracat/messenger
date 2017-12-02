25697972ff3d0a76624b2dfc70169f4a9b1efdb0
Extracat <snezhi@MBP-Sabina.Dlink>
2017-12-03 01:16:59 +0300

    full users api

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // =======================
-// get the packages we need ============
+// get the packages we need 
 // =======================
 
 var os = require("os")
@@ -19,16 +19,12 @@ var User   = require('./app/models/user'); // get our mongoose model
 
 
 // =======================
-// configuration =========
+// configuration 
 // =======================
 
-
 var port = process.env.PORT || 8080
-
 mongoose.connect(config.database, {useMongoClient: true}); // connect to database
 app.set('superSecret', config.secret); // secret variable
-
-
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,9 +33,13 @@ app.use(bodyParser.json());
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 
+
+
 // =======================
-// routes ================
+// routes 
 // =======================
+
+
 // basic route
 app.get('/', function(req, res) {
     res.send('Hello, World!');
@@ -51,47 +51,87 @@ app.get('/', function(req, res) {
 // get an instance of the router for api routes
 var apiRoutes = express.Router(); 
 
-// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+// route to authenticate a user
 apiRoutes.post('/authenticate', function(req, res) {
 
   // find the user
-  User.findOne({
-    name: req.body.name
-  }, function(err, user) {
+  User.findOne({username: req.body.username}, function(err, user) {
 
-    if (err) throw err;
+    if (err) {
+            res.json({
+                success: false,
+                message: "Error occured: " + err
+            });
+    } else {
 
-    if (!user) {
-      res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (user) {
+        if (!user) {
+          res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
 
-      // check if password matches
-      if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-      } else {
+          // check if password matches
+          if (user.password != req.body.password) {
+            res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+          } else {
 
-        // if user is found and password is right
-        // create a token with only our given payload
-    // we don't want to pass in the entire user since that has the password
-    const payload = {
-      admin: user.admin 
-    };
-        var token = jwt.sign(payload, app.get('superSecret'), {
-          expiresIn: '60m' // expires in 
-        });
 
-        // return the information including token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }   
-
-    }
-
-  });
+            var token = jwt.sign(user.id, app.get('superSecret'));
+            // if user is found and password is right
+            // return the information including token as JSON
+            res.json({
+              success: true,
+              id: user.id,
+              token: token
+            });
+          }   
+        }
+  }});
 });
+
+// route to sign up new user
+apiRoutes.post('/signup', function(req, res) {
+
+    if (req.body.username != null && req.body.password != null) {
+
+      User.findOne({username: req.body.username}, function(err, user) {
+        if (err) {
+            res.json({
+                success: false,
+                message: "Error occured: " + err
+            });
+        } else {
+            if (user) {
+                res.json({
+                    success: false,
+                    message: "User already exists!"
+                });
+            } else {
+                var newUser = new User();
+                newUser.username = req.body.username;
+                newUser.password = req.body.password;
+               
+                newUser.save(function(err, user) {
+                        if (err) return console.error(err);
+
+                        var token = jwt.sign(user.id, app.get('superSecret'));
+                        res.json({
+                            success: true,
+                            id: user.id,
+                            token: token
+                        });
+                })
+            }
+        }
+    });
+  }
+  else {
+    res.json({
+                success: false,
+                message: "Bad request"
+            });
+  }
+});
+
+
 
 
 // route middleware to verify a token
@@ -107,10 +147,31 @@ apiRoutes.use(function(req, res, next) {
     jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
       if (err) {
         return res.json({ success: false, message: 'Failed to authenticate token.' });    
+
+
       } else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;    
-        next();
+        User.findById(decoded, function(err, existingUser) {
+                if (err) {
+                    res.json({
+                        success: false,
+                        message: "Error occured: " + err
+                    });
+                } else {
+                    if (existingUser && existingUser.id == decoded) {
+
+                        // if everything is good, save to request for use in other routes
+                        req.decoded = decoded;    
+                        next();
+
+                    }
+                    else {
+                      return res.json({ success: false, message: 'The user does not exist'});    
+
+
+                    }
+
+                  }
+        });
       }
     });
 
@@ -130,15 +191,123 @@ apiRoutes.use(function(req, res, next) {
 
 // route to show a random message (GET http://localhost:8080/api/)
 apiRoutes.get('/', function(req, res) {
-  res.json({ message: 'Welcome to the coolest API on earth!' });
+  res.json({ id: req.decoded});
 });
 
-// route to return all users (GET http://localhost:8080/api/users)
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+
+// route to return all users 
 apiRoutes.get('/users', function(req, res) {
   User.find({}, function(err, users) {
     res.json(users);
   });
 });   
+
+
+
+apiRoutes.get('/users/:id', function(req, res) {
+    
+  var id = req.params.id; 
+    
+  User.findById(id, function(err, user) {
+        
+    if(user){
+        res.json(user);
+    }
+    else{
+        res.status(404).send();
+    }
+  });
+});   
+
+
+apiRoutes.post('/users', function(req, res) {
+
+    if (req.body.username != null && req.body.password != null) {
+
+      User.findOne({username: req.body.username}, function(err, user) {
+        if (err) {
+            res.json({
+                success: false,
+                message: "Error occured: " + err
+            });
+        } else {
+            if (user) {
+                res.json({
+                    success: false,
+                    message: "User already exists!"
+                });
+            } else {
+                var newUser = new User();
+                newUser.username = req.body.username;
+                newUser.password = req.body.password;
+               
+                newUser.save(function(err, user) {
+                        if (err) return console.error(err);
+                        res.json(user);
+                })
+            }
+        }
+    });
+  }
+  else {
+    res.json({
+                success: false,
+                message: "Bad request"
+            });
+  }
+
+
+ 
+}); 
+
+
+apiRoutes.put('/users/:id', function(req, res) {
+
+  var id = req.params.id; 
+    
+  User.findByIdAndUpdate(id, {username: req.body.username, password: req.body.password}, function(err, user) {
+        
+    if(user){
+          User.findById(id, function(err, user2) {res.json(user2)});
+    }
+    else{
+        res.status(404).send();
+    }
+  });
+ 
+});
+
+
+apiRoutes.delete('/users/:id', function(req, res) {
+  var id = req.params.id; 
+    
+  User.findById(id, function(err, user) {
+        
+    if(user){
+        user.remove(function (err) {
+             if (err) return handleError(err);
+              res.json(user);    
+         });        
+    }
+    else{
+        res.status(404).send();
+    }
+  });
+}); 
+ 
+
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
 
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
@@ -147,20 +316,13 @@ app.use('/api', apiRoutes);
 
 app.get('/setup', function(req, res) {
 
-  // create a sample user
-  var user = new User({ 
-    name: 'user', 
-    password: 'password',
-    admin: true 
-  });
+  User.remove({}, function (err) {
+    if (err) return handleError(err);
 
-  // save the sample user
-  nick.save(function(err) {
-    if (err) throw err;
+    return res.json({message: 'All users was removed'});    
 
-    console.log('User saved successfully');
-    res.json({ success: true });
   });
+  
 });
 
 
